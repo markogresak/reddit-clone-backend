733e943fbc5adfa00a4f0f9f8c9b967f2d9f59d3
Marko Grešak <gresak.marko@gmail.com>
2017-01-31 10:27:09 +0100

    Ask before running seeds, update code to avoid username collisions

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -16,16 +16,31 @@ defmodule RedditClone.Seeds do
   alias RedditClone.User
   alias RedditClone.Post
   alias RedditClone.Comment
+  import Ecto.Query, only: [from: 2]
 
   defp random(min, max) do
     :rand.uniform(max - min + 1) + (min - 1)
   end
 
+  defp find_last_user_number() do
+    query = from u in User, where: like(u.username, "user%"), select: u.username
+    usernames = Repo.all(query)
+    Enum.map(usernames, fn(username) -> String.split(username, "user") |> List.last |> String.to_integer end)
+    |> Enum.sort(&(&1 >= &2))
+    |> List.first
+  end
+
   defp insert_user(i) do
-    Repo.insert! %User{
-      username: "user" <> to_string(i),
-      encrypted_password: Comeonin.Bcrypt.hashpwsalt("pass1234"),
-    }
+    username = "user" <> to_string(i)
+    if (Repo.get_by(User, username: username) != nil) do
+      # recursively find first free username
+      insert_user(i + 1)
+    else
+      Repo.insert! %User{
+        username: username,
+        encrypted_password: Comeonin.Bcrypt.hashpwsalt("asdf1234"),
+      }
+    end
   end
 
   defp insert_post_with_url(i, post_user) do
@@ -55,27 +70,39 @@ defmodule RedditClone.Seeds do
   end
 
   def run() do
-    IO.puts "Seeding users"
-    # insert 10 users
-    users = Enum.map(1..10, &(insert_user(&1)))
+    IO.puts "This will generate 10 users and posts + comments. It will recursively search for usernames user{i}, which might be slow."
+    user_input = String.trim(IO.gets "Are you sure you want to proceed? [y/n] ")
+    if (user_input == "y") do
+      IO.puts "Seeding users"
+      last_user_number = find_last_user_number() || 1
+      # insert 10 users
+      users = Enum.map(last_user_number..(last_user_number + 9), &(insert_user(&1)))
 
-    IO.puts "Seeding posts"
-    # iterate 10 times, each time insert 2-5 posts for a total of 20-50 posts
-    posts = Enum.flat_map(1..10, fn(i) ->
-      insert_fn = if (rem(i, 2) == 0), do: &insert_post_with_url/2, else: &insert_post_with_text/2
-      post_user = Enum.random(users)
-      Enum.map(1..(random(2, 5)), &(insert_fn.(i * 10 + &1, post_user)))
-    end)
+      IO.puts "Seeding posts"
+      # iterate 10 times, each time insert 2-5 posts for a total of 20-50 posts
+      posts = Enum.flat_map(1..10, fn(i) ->
+        insert_fn = if (rem(i, 2) == 0), do: &insert_post_with_url/2, else: &insert_post_with_text/2
+        post_user = Enum.random(users)
+        Enum.map(1..(random(2, 5)), &(insert_fn.(i * 10 + &1, post_user)))
+      end)
 
-    IO.puts "Seeding comments"
-    # for each post, insert random number of comments
-    Enum.each(posts, fn(post) ->
-      number_of_comments = random(0, 20)
-      if (number_of_comments > 0) do
-        comment_user = Enum.random(users)
-        Enum.each(1..number_of_comments, fn(_i) -> insert_comment(comment_user, post) end)
-      end
-    end)
+      IO.puts "Seeding comments"
+      # for each post, insert random number of comments
+      _comments = Enum.flat_map(posts, fn(post) ->
+        number_of_comments = random(0, 20)
+        if (number_of_comments > 0) do
+          comment_user = Enum.random(users)
+          Enum.map(1..number_of_comments, fn(_i) -> insert_comment(comment_user, post) end)
+        else
+          # return [] to avoid returning nil, which breaks Enum.flat_map
+          []
+        end
+      end)
+
+      IO.puts "Inserted users:"
+      Enum.each(users, fn(user) -> IO.puts "\t#{user.username}" end)
+      IO.puts "All usernames above have password: asdf1234"
+    end
   end
 end
 
