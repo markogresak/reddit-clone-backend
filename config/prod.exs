300c547964d31825843534d10def6cee4c504a24
Marko Grešak <gresak.marko@gmail.com>
2017-01-31 08:58:00 +0100

    Remove cache manifest

--- config/prod.exs
+++ config/prod.exs
@@ -15,7 +15,6 @@ config :reddit_clone, RedditClone.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [scheme: "https", host: System.get_env("HEROKU_URL"), port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 config :reddit_clone, RedditClone.Repo,
