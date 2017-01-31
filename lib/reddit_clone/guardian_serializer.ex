a17eb4f2aadb3ffcb677d091458fdacff7c7d278
Marko Grešak <gresak.marko@gmail.com>
2017-01-31 08:43:44 +0100

    Add files previously ignored by global gitignore
    I don't even...

---
+++ lib/reddit_clone/guardian_serializer.ex
@@ -0,0 +1,12 @@
+defmodule RedditClone.GuardianSerializer do
+  @behaviour Guardian.Serializer
+
+  alias RedditClone.Repo
+  alias RedditClone.User
+
+  def for_token(user = %User{}), do: { :ok, "User:#{user.id}" }
+  def for_token(_), do: { :error, "Unknown resource type" }
+
+  def from_token("User:" <> id), do: { :ok, Repo.get(User, id) }
+  def from_token(_), do: { :error, "Unknown resource type" }
+end
---
+++ lib/reddit_clone/repo.ex
@@ -0,0 +1,3 @@
+defmodule RedditClone.Repo do
+  use Ecto.Repo, otp_app: :reddit_clone
+end
